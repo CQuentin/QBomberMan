a976a2ec5104e6b991b9e01cb4daa1d98037a341
CHEYNET Quentin <c20903125@tempo21-2.luminy.univ-amu.fr>
2013-11-22 21:01:10 +0100

    ajout du main

--- code/Game1/main.cpp
+++ code/Game1/main.cpp
@@ -1,13 +1,11 @@
-#include "vue/mainwindow.h"
+#include "qbomberman.h"
 #include <QApplication>
+#include "./vue/mainwindow.h"
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	MainWindow w;
-	w.setWindowTitle(QString::fromUtf8("Game1"));
-	w.resize(900, 600);
-	w.show();
-
-	return a.exec();
+    QApplication a(argc, argv);
+    QBomberMan w;
+    w.show();
+    return a.exec();
 }
