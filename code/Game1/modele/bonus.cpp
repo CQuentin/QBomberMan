cdb031f18dbcc0e9235f03ff2870f23f0fbb185d
Quentin <quentin@dQuentin.domaine.q.fr>
2013-11-24 11:08:57 +0100

    maj de la classe Bonus

--- code/Game1/modele/bonus.cpp
+++ code/Game1/modele/bonus.cpp
@@ -1,5 +1,33 @@
 #include "bonus.h"
 
-Bonus::Bonus()
-{
+Bonus::Bonus(int x, int y){
+    posX = x;
+    posY = y;
+
+    type = qrand()%6;
+
+    // TODO sprite
+    switch(type){
+    case HP :
+        break;
+    case EXPLOSION :
+        break;
+    case BOMBE :
+        break;
+    case JUMP :
+        break;
+    case SPEED :
+        break;
+    case TRIGGER :
+        break;
+    }
+
+}
+
+int Bonus::getBonusType(){
+    return type;
+}
+
+QGraphicsPixmapItem* Bonus::getPicture(){
+    return picture;
 }
--- code/Game1/modele/bonus.h
+++ code/Game1/modele/bonus.h
@@ -1,10 +1,48 @@
 #ifndef BONUS_H
 #define BONUS_H
 
+#include <QGraphicsPixmapItem>
+#include <QPixmap>
 class Bonus
 {
+
+private :
+    enum types
+    {
+        HP =0,
+        EXPLOSION =1,
+        BOMBE = 2,
+        JUMP = 3,
+        SPEED = 4,
+        TRIGGER = 5
+    };
+    int posX;
+    int posY;
+    int type;
+    QGraphicsPixmapItem *picture;
+
+
 public:
-    Bonus();
+
+    Bonus(int x ,int y);
+
+
+    /*!
+     * \brief renvoie le type de bonus
+     * \return type
+     * \author Quentin CHEYNET
+     */
+    int getBonusType();
+
+    /*!
+     * \brief renvoie l'attribut picture
+     * \return picture
+     * \author Quentin CHEYNET
+     */
+    QGraphicsPixmapItem *getPicture();
+
+    ~Bonus();
+
 };
 
 #endif // BONUS_H
