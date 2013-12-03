d8ae1ea2b8a1731e80baa0c40be970d06b38ad76
Quentin <cheynet.q@gmail.com>
2013-12-03 11:47:24 +0100

    ctrl-i

--- code/Game1/controleur/toucheclavier.cpp
+++ code/Game1/controleur/toucheclavier.cpp
@@ -11,24 +11,24 @@ ToucheClavier::ToucheClavier()
 
 void ToucheClavier::setPressed(int key, bool state){
 
-        switch(key){
-        case Qt::Key_Up:
-            stateKeys[0] = state;
-            break;
-        case Qt::Key_Left:
-            stateKeys[1] = state;
-            break;
-        case Qt::Key_Down:
-            stateKeys[2] = state;
-            break;
-        case Qt::Key_Right:
-            stateKeys[3] = state;
-            break;
-        case Qt::Key_Space:
-            stateKeys[4] = state;
-            break;
-        default : break;
-        }
+    switch(key){
+    case Qt::Key_Up:
+        stateKeys[0] = state;
+        break;
+    case Qt::Key_Left:
+        stateKeys[1] = state;
+        break;
+    case Qt::Key_Down:
+        stateKeys[2] = state;
+        break;
+    case Qt::Key_Right:
+        stateKeys[3] = state;
+        break;
+    case Qt::Key_Space:
+        stateKeys[4] = state;
+        break;
+    default : break;
+    }
 }
 
 bool ToucheClavier::getStateKeys(int i){
